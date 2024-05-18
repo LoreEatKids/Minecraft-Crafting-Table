5f2d5bcc5fed2fd0df1f0aa36ae9ad72f04c0331
LoreEatKids <lorenzo.galetto@icloud.com>
2024-05-18 11:50:00 +0200

    DRY code with class and fixed bugs

--- app.js
+++ app.js
@@ -1,85 +1,102 @@
 "use strict";
 
-let CRAFTING_TABLE = 4;
-let WOOD_PLANK = 5;
-let STICK = 30;
-let WOOD = 17;
-let WOOD_PICKAXE = 40;
-let STONE_PICKAXE = 41;
-let COBBLESTONE = 6;
+class CraftingTable {
+  constructor() {
+    this.CRAFTING_TABLE = 4;
+    this.WOOD_PLANK = 5;
+    this.STICK = 30;
+    this.WOOD = 17;
+    this.WOOD_PICKAXE = 40;
+    this.STONE_PICKAXE = 41;
+    this.COBBLESTONE = 6;
 
-const inventoryContailerEl = document.querySelector(".items-container");
-const dataCellsAll = document.querySelectorAll(".grid-element");
-const invContainerEl = document.querySelector(".items-container");
-const resultEl = document.querySelector(".result");
+    this.inventoryContainerEl = document.querySelector(".items-container");
+    this.dataCellsAll = document.querySelectorAll(".grid-element");
+    this.resultEl = document.querySelector(".result");
 
-const currentCraftingTable = [];
-const inventory = [WOOD_PLANK, COBBLESTONE, 0, 0, 0, 0, 0].sort(x => x > 0);
+    this.currentCraftingTable = [];
+    this.inventory = [this.WOOD_PLANK, this.COBBLESTONE].sort((x) => x > 0);
 
-const crafting = [
-    [[0, 0, 0, 0, WOOD_PLANK, 0, 0, 0 ,0], WOOD],  // TODO multiple wood crafting: [[WOOD_PLANK, 0, 0, 0, 0, 0, 0, 0, 0], WOOD], [[0, WOOD_PLANK, 0, 0, 0, 0, 0, 0, 0], WOOD], [[0, 0, WOOD_PLANK, 0, 0, 0, 0, 0, 0], WOOD],
-    [[0, 0, 0, 0, WOOD, 0, 0, WOOD ,0], STICK],
-    [[WOOD, WOOD, WOOD, 0, STICK, 0, 0, STICK, 0], WOOD_PICKAXE],
-    [[COBBLESTONE, COBBLESTONE, COBBLESTONE, 0, STICK, 0, 0, STICK, 0], STONE_PICKAXE],
-    [[0, 0, 0, WOOD, WOOD, 0, WOOD, WOOD, 0], CRAFTING_TABLE]
-];
+    this.crafting = [
+      [[0, 0, 0, 0, this.WOOD_PLANK, 0, 0, 0, 0], this.WOOD],
+      [[0, 0, 0, 0, this.WOOD, 0, 0, this.WOOD, 0], this.STICK],
+      [[this.WOOD, this.WOOD, this.WOOD, 0, this.STICK, 0, 0, this.STICK, 0], this.WOOD_PICKAXE],
+      [[this.COBBLESTONE, this.COBBLESTONE, this.COBBLESTONE, 0, this.STICK, 0, 0, this.STICK, 0], this.STONE_PICKAXE],
+      [[0, 0, 0, this.WOOD, this.WOOD, 0, this.WOOD, this.WOOD, 0], this.CRAFTING_TABLE]
+    ];
 
-let selectedItems = 0;
+    this.selectedItems = 0;
 
-const loadInv = inv => {
-    invContainerEl.innerHTML = ""; // refreshing inv items
+    this.loadInventory();
+    this.setupEventListeners();
+  }
 
-    inv.forEach(item => {
-        const html = item > 0 ? 
-        `<div class="items-element"><img src="static/${item + ".png"}" class="inventory-img" data-id="${item}"></div>` :
-        `<div class="items-element" data-id="0">0</div>`;
-        inventoryContailerEl.insertAdjacentHTML("afterbegin", html);
+  loadInventory() {
+    this.inventoryContainerEl.innerHTML = ""; // Refreshing inventory items
+    this.inventory.forEach((item) => {
+      const html =
+        item > 0
+          ? `<div class="items-element"><img src="static/${item}.png" class="inventory-img" data-id="${item}"></div>`
+          : `<div class="items-element" data-id="0">0</div>`;
 
-        // refresh event
-        document.querySelectorAll(".inventory-img").forEach(cell => cell.addEventListener("click", ()=> {if (cell) selectedItems = Number(cell.dataset.id)}));
+      this.inventoryContainerEl.insertAdjacentHTML("afterbegin", html);
     });
+
+    // Refresh event
+    document.querySelectorAll(".inventory-img").forEach((cell) =>
+      cell.addEventListener("click", () => { if (cell) this.selectedItems = Number(cell.dataset.id); })
+    );
+  }
+
+  createItemImg(cell, path) {
+    cell.innerHTML = "";
+    const img = document.createElement("img");
+    img.classList.add("img-class");
+    img.src = `static/${path}.png`;
+    img.dataset.id = path;
+    cell.appendChild(img);
+  }
+
+  updateCrafting(craft) {
+    this.currentCraftingTable = Array.from(craft, (x) => Number(x.dataset.id));
+    this.checkAndHandleCrafting();
+  }
+
+  checkAndHandleCrafting() {
+    const matchedCraft = this.crafting.find( (craft) => this.currentCraftingTable.join() === craft[0].join() );
+    if (matchedCraft) this.displayCraftingResult(matchedCraft[1]);
+    else this.resultEl.innerHTML = "";
+  }
+
+  displayCraftingResult(newCraft) {
+    this.resultEl.innerHTML = "";
+    this.createItemImg(this.resultEl, newCraft);
+
+    const img = this.resultEl.querySelector("img");
+
+    img.addEventListener("click", () => {
+      if (this.inventory.includes(newCraft)) {
+        alert("You already have this item");
+        return;
+      }
+
+      this.inventory.push(newCraft);
+      this.loadInventory();
+    });
+  }
+
+  setupEventListeners() {
+    this.dataCellsAll.forEach((cell) => cell.addEventListener("click", this.handleCellClick.bind(this, cell)) );
+  }
+
+  handleCellClick(cell) {
+    if (this.selectedItems === 0) return;
+
+    cell.dataset.id = this.selectedItems;
+
+    this.updateCrafting(this.dataCellsAll);
+    this.createItemImg(cell, cell.dataset.id);
+  }
 }
-loadInv(inventory)
 
-const updateCrafting = craft => {
-    currentCraftingTable.length = 0; // reset the crafting each time you click
-
-    craft.forEach(x => currentCraftingTable.push(Number(x.dataset.id)));
-    checkMatch(currentCraftingTable);
-}
-
-const checkMatch = recipe => {
-    crafting.forEach(craft => {
-        if (currentCraftingTable.join() == craft[0]) {
-            const newCraft = craft[1];
-            resultEl.innerHTML = "";
-
-            const img = document.createElement("img");
-            img.classList.add("img-class");
-            img.src = `static/${newCraft}.png`;
-            resultEl.appendChild(img);
-
-            img.addEventListener("click", () => {
-                inventory.push(newCraft);            
-                loadInv(inventory);
-            })
-        }
-    })
-}
-
-dataCellsAll.forEach(x => {
-    x.addEventListener("click", () => {
-        if (selectedItems != 0) {
-            x.dataset.id = selectedItems;
-            
-            updateCrafting(document.querySelectorAll('.grid-element'));
-                
-            x.innerHTML = "";
-            const img = document.createElement("img");
-            img.classList.add("img-class");
-            img.dataset.id = x.dataset.id;
-            img.src = `static/${x.dataset.id}.png`;
-            x.appendChild(img);
-        } else alert("select an item before");
-    })
-});
+const MinecraftCraftingTable = new CraftingTable();
